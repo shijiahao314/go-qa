c70d3b94da2fc1f4e3a4582e43560f6660f5eb1e
shijiahao <shijiahao314@foxmail.com>
2023-12-09 21:07:37 +0800

    modify

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log/slog"
 
 	"github.com/shijiahao314/go-qa/global"
 	"github.com/shijiahao314/go-qa/setup"
+	"go.uber.org/zap"
 )
 
 func main() {
@@ -13,6 +13,6 @@ func main() {
 
 	r := setup.InitRouter()
 	if err := r.Run(fmt.Sprintf("0.0.0.0:%d", global.Config.Port)); err != nil {
-		slog.Error("failed to run server on 0.0.0.0", slog.Int("port", global.Config.Port), err)
+		global.Logger.Error("failed to run server", zap.String("host", "0.0.0.0"), zap.Int("port", global.Config.Port), zap.String("err", err.Error()))
 	}
 }
