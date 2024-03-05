d8b38fae3cb19e11515e8073c2abf767ca4c7bd3
shijiahao <shijiahao314@foxmail.com>
2024-03-05 22:35:59 +0800

    save

--- test/test.go
+++ test/test.go
@@ -1,21 +1,24 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"slices"
-	"sort"
-)
-
 func main() {
-	n := make([]int, 10, 10)
-	for idx := range n {
-		n[idx] = rand.Intn(999)
-	}
-	fmt.Println(n)
-	sort.Ints(n)
-	fmt.Println(n)
-	slices.Reverse(n)
-	fmt.Println(n)
 
+}
+
+func append0(tmp []int) {
+	ans := make([][]int, len(tmp))
+	for idx := range tmp {
+		ans[0][idx] = tmp[idx]
+	}
+}
+
+func append1(tmp []int) {
+	ans := make([][]int, 0)
+	ans = append(ans, append([]int{}, tmp...))
+}
+
+func append2(tmp []int) {
+	ans := make([][]int, 0)
+	tmpCopy := make([]int, len(tmp))
+	copy(tmpCopy, tmp)
+	ans = append(ans, tmpCopy)
 }
