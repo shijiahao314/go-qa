496e5c82bb0ec8a7975d1b156e22816ce4532792
shijiahao <shijiahao314@foxmail.com>
2024-10-07 15:15:16 +0800

    use random string

--- bootstrap/router.go
+++ bootstrap/router.go
@@ -1,81 +1,83 @@
 package bootstrap
 
 import (
+	"crypto/rand"
 	"log/slog"
+	"math/big"
 
 	"github.com/boj/redistore"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
 	"github.com/gin-contrib/sessions/redis"
 	"github.com/gin-gonic/gin"
-
-	// gSessions "github.com/gorilla/sessions"
 	"github.com/shijiahao314/go-qa/global"
 	"github.com/shijiahao314/go-qa/router"
 )
 
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[]{}<>?/"
+
+// generateRandomString 生成随机字符串
+func generateRandomString(length int) (string, error) {
+	result := make([]byte, length)
+	charsetLength := big.NewInt(int64(len(charset)))
+
+	for i := 0; i < length; i++ {
+		// 从字符集中随机选择一个字符
+		index, err := rand.Int(rand.Reader, charsetLength)
+		if err != nil {
+			return "", err
+		}
+		result[i] = charset[index.Int64()]
+	}
+
+	return string(result), nil
+}
+
 const (
-	HealthPath = "/api/health"
-	SecretKey  = "iV6pNvjdHVUVc5Q*Wi4S&" // random
+	HealthPath      = "/api/health"
+	SecretKeyLength = 32
 )
-
-// type MySerializer struct{}
-
-// func (ms MySerializer) Serialize(ss *gSessions.Session) ([]byte, error) {
-// 	m := make(map[string]interface{}, len(ss.Values))
-// 	for k, v := range ss.Values {
-// 		ks, ok := k.(string)
-// 		if !ok {
-// 			err := fmt.Errorf("Non-string key value, cannot serialize session to JSON: %v", k)
-// 			fmt.Printf("redistore.JSONSerializer.serialize() Error: %v", err)
-// 			return nil, err
-// 		}
-// 		fmt.Printf("[%s]=[%s]\n", k, v)
-// 		m[ks] = v
-// 	}
-// 	return json.Marshal(m)
-// }
-
-// func (ms MySerializer) Deserialize(d []byte, ss *gSessions.Session) error {
-// 	m := make(map[string]interface{})
-// 	err := json.Unmarshal(d, &m)
-// 	if err != nil {
-// 		fmt.Printf("redistore.JSONSerializer.deserialize() Error: %v", err)
-// 		return err
-// 	}
-// 	for k, v := range m {
-// 		fmt.Printf("[%s]=[%s]\n", k, v)
-// 		ss.Values[k] = v
-// 	}
-// 	return nil
-// }
 
 // MustInitRouter 初始化路由配置
 func MustInitRouter() *gin.Engine {
 	r := gin.New()
 
-	// session
-	store, err := redis.NewStore(
-		global.Config.Redis.ConnectionNum,
-		"tcp",
-		global.Config.Redis.Addr,
-		global.Config.Redis.Password,
-		[]byte(SecretKey),
-	)
+	secretKey, err := generateRandomString(SecretKeyLength)
 	if err != nil {
-		slog.Error("failed to init redis", slog.String("err", err.Error()))
+		slog.Error("failed to generate random string", slog.String("err", err.Error()))
 		panic(err)
 	}
-	_, rs := redis.GetRedisStore(store)
 
-	rs.SetSerializer(redistore.JSONSerializer{})
+	// session
+	var store sessions.Store
+	if global.Redis != nil {
+		// Redis 可用则使用
+		store, err := redis.NewStore(
+			global.Config.Redis.ConnectionNum,
+			"tcp",
+			global.Config.Redis.Addr,
+			global.Config.Redis.Password,
+			[]byte(secretKey),
+		)
+		if err != nil {
+			slog.Error("failed to init redis", slog.String("err", err.Error()))
+			panic(err)
+		}
+		_, rs := redis.GetRedisStore(store)
 
-	store.Options(sessions.Options{
-		Path:     "/",
-		MaxAge:   60 * 60 * 24,
-		Secure:   false,
-		HttpOnly: false,
-	})
+		rs.SetSerializer(redistore.JSONSerializer{})
+
+		store.Options(sessions.Options{
+			Path:     "/",
+			MaxAge:   60 * 60 * 24,
+			Secure:   false,
+			HttpOnly: false,
+		})
+	} else {
+		// 如果无法使用 Redis 则使用 cookie
+		store = cookie.NewStore([]byte(secretKey))
+	}
 	r.Use(sessions.Sessions("session", store))
 
 	r.Use(
