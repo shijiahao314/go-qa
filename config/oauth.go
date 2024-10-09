5c40f87967c596a2f15611202fcb213d239938af
shijiahao <shijiahao314@foxmail.com>
2024-10-09 11:56:31 +0800

    update config

--- config/oauth.go
+++ config/oauth.go
@@ -7,5 +7,4 @@ type OAuthConfig struct {
 type GithubConfig struct {
 	ClientID     string `mapstructure:"client-id" json:"client-id" yaml:"client-id"`
 	ClientSecret string `mapstructure:"client-secret" json:"client-secret" yaml:"client-secret"`
-	RedirectURL  string `mapstructure:"redirect-url" json:"redirect-url" yaml:"redirect-url"`
 }
